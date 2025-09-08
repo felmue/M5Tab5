4682dd726b45a41c26ee33dcd52f91e6c835d38f
fm <felix.mueller@gwendesign.ch>
2025-09-08 10:23:27 +0200

    Fix compilation and adapt to M5Unified version 0.2.8

--- src/M5Tab5.cpp
+++ src/M5Tab5.cpp
@@ -17,8 +17,6 @@ void M5_TAB5::begin(bool clearAndDisableRTCInterrupt)
 //    M5.begin();
 //    i2c_init();
     io_expander_a_init();
-    io_expander_b_init();
-    ina226_init();
     rx8130_init(clearAndDisableRTCInterrupt);
 
     pinMode(M5TAB5_STAMP_RX_PIN, INPUT);
@@ -28,31 +26,6 @@ void M5_TAB5::begin(bool clearAndDisableRTCInterrupt)
 void M5_TAB5::update()
 {
     M5.update();
-}
-
-/* -------------------------------------------------------------------------- */
-/*                                   INA226                                   */
-/* -------------------------------------------------------------------------- */
-void M5_TAB5::ina226_init()
-{
-    if (!INA226.begin()) {
-        ESP_LOGE(TAG, "ina226 init failed");
-    } else {
-        // 28.4 Hz
-        INA226.configure(INA226_AVERAGES_16, INA226_BUS_CONV_TIME_1100US, INA226_SHUNT_CONV_TIME_1100US,
-                         INA226_MODE_SHUNT_BUS_CONT);
-        INA226.calibrate(0.005, 8.192);
-    }
-}
-
-float M5_TAB5::getBatteryVoltage()
-{
-    return INA226.readBusVoltage();
-}
-
-float M5_TAB5::getBatteryCurrent()
-{
-    return INA226.readShuntCurrent();
 }
 
 /* -------------------------------------------------------------------------- */
@@ -117,167 +90,50 @@ uint8_t M5_TAB5::readRtcReg(uint8_t reg)
 }
 
 /* -------------------------------------------------------------------------- */
-/*                                  IO EXT A                                  */
+/*                                  IO EXT A (0)                              */
 /* -------------------------------------------------------------------------- */
 void M5_TAB5::setRFPath(bool l_int_h_ext)
 {
-    _io_expander_a->digitalWrite(0, l_int_h_ext);
+    auto& ioe = M5.getIOExpander(0);
+    ioe.digitalWrite(0, l_int_h_ext);
 }
 
 void M5_TAB5::setSpeakerEnable(bool enable)
 {
-    _io_expander_a->digitalWrite(1, enable);
-}
-
-void M5_TAB5::setExt5VEnable(bool enable)
-{
-    _io_expander_a->digitalWrite(2, enable);
+    auto& ioe = M5.getIOExpander(0);
+    ioe.digitalWrite(1, enable);
 }
 
 void M5_TAB5::setStamp5VEnable(bool enable)
 {
-    _io_expander_a->digitalWrite(3, enable);
+    auto& ioe = M5.getIOExpander(0);
+    ioe.digitalWrite(3, enable);
 }
 
 void M5_TAB5::io_expander_a_init()
 {
-    _io_expander_a = new PI4IOE5V6408_Class;
-    if (!_io_expander_a->begin()) {
-        ESP_LOGE(TAG, "io expander a init failed");
-    } else {
-        _io_expander_a->resetIrq();
-        // P0 : RF_PTH_L_INT_H_EXT : LOW -> internal antenna
-        _io_expander_a->setDirection(0, true);
-        _io_expander_a->setPullMode(0, false);
-        _io_expander_a->setHighImpedance(0, false);
-        _io_expander_a->digitalWrite(0, false);
-        // P1 : SPK_EN : HIGH -> enable
-        _io_expander_a->setDirection(1, true);
-        _io_expander_a->setPullMode(1, false);
-        _io_expander_a->setHighImpedance(1, false);
-        _io_expander_a->digitalWrite(1, true);
-        // P2 : EXT5V_EN : HIGH -> enable
-        _io_expander_a->setDirection(2, true);
-        _io_expander_a->setPullMode(2, false);
-        _io_expander_a->setHighImpedance(2, false);
-        _io_expander_a->digitalWrite(2, true);
-        // P3 : STAMP5V_EN : LOW -> disabled
-        _io_expander_a->setDirection(3, true);
-        _io_expander_a->setPullMode(3, false);
-        _io_expander_a->setHighImpedance(3, false);
-        _io_expander_a->digitalWrite(3, false);
-        // P4 : LCD_RST : HIGH
-        _io_expander_a->setDirection(4, true);
-        _io_expander_a->setPullMode(4, false);
-        _io_expander_a->setHighImpedance(4, false);
-        _io_expander_a->digitalWrite(4, true);
-        // P5 : TP_RST : HIGH
-        _io_expander_a->setDirection(5, true);
-        _io_expander_a->setPullMode(5, false);
-        _io_expander_a->setHighImpedance(5, false);
-        _io_expander_a->digitalWrite(5, true);
-        // P6 : CAM_RST : HIGH
-        _io_expander_a->setDirection(6, true);
-        _io_expander_a->setPullMode(6, false);
-        _io_expander_a->setHighImpedance(6, false);
-        _io_expander_a->digitalWrite(6, true);
-        // P7 : HP_DET
-        _io_expander_a->setDirection(7, false);
-        _io_expander_a->setPullMode(7, true);
-        _io_expander_a->setHighImpedance(7, false);
-        delay(100);
-    }
+    auto& ioe = M5.getIOExpander(0);
+    // P3 : STAMP5V_EN : LOW -> disabled
+    ioe.setDirection(3, true);
+    ioe.setPullMode(3, false);
+    ioe.setHighImpedance(3, false);
+    ioe.digitalWrite(3, false);
 }
 
 /* -------------------------------------------------------------------------- */
-/*                                  IO EXT B                                  */
+/*                                  IO EXT B (1)                              */
 /* -------------------------------------------------------------------------- */
 void M5_TAB5::setWLANPowerEnable(bool enable)
 {
-    _io_expander_b->digitalWrite(0, enable);
-}
-
-void M5_TAB5::setUSB5VEnable(bool enable)
-{
-    _io_expander_b->digitalWrite(3, enable);
-}
-
-void M5_TAB5::powerOff()
-{
-    M5.Display.sleep();
-    M5.Display.waitDisplay();
-    for(int i = 0; i < 3; i++)
-    {
-        _io_expander_b->digitalWrite(4, true);
-        delay(100);
-        _io_expander_b->digitalWrite(4, false);
-        delay(100);
-    }
+    auto& ioe = M5.getIOExpander(1);
+    ioe.digitalWrite(0, enable);
 }
 
 void M5_TAB5::setQuickChargeEnable(bool enable)
 {
     // low: quick charge on
     // high: quick charge off
-    _io_expander_b->digitalWrite(5, !enable);
-}
-
-bool M5_TAB5::getChargeState()
-{
-    if(_io_expander_b->digitalRead(6) == 1) return true;
-    return false;
-}
-
-void M5_TAB5::setChargerEnable(bool enable)
-{
-    _io_expander_b->digitalWrite(7, enable);
-}
-
-void M5_TAB5::io_expander_b_init()
-{
-    _io_expander_b = new PI4IOE5V6408_Class(0x44);
-    if (!_io_expander_b->begin()) {
-        ESP_LOGE(TAG, "io expander b init failed");
-    } else {
-        _io_expander_b->resetIrq();
-        // P0 : WLAN_PWR_EN : HIGH -> enable
-        _io_expander_b->setDirection(0, true);
-        _io_expander_b->setPullMode(0, false);
-        _io_expander_b->setHighImpedance(0, false);
-        _io_expander_b->digitalWrite(0, true);
-        // P1 : unused
-        _io_expander_b->setDirection(1, false);
-        _io_expander_b->setPullMode(1, true);
-        _io_expander_b->setHighImpedance(1, true);
-        // P2 : unused
-        _io_expander_b->setDirection(2, false);
-        _io_expander_b->setPullMode(2, true);
-        _io_expander_b->setHighImpedance(2, true);
-        // P3 : USB5V_EN : HIGH -> enable
-        _io_expander_b->setDirection(3, true);
-        _io_expander_b->setPullMode(3, false);
-        _io_expander_b->setHighImpedance(3, false);
-        _io_expander_b->digitalWrite(3, true);
-        // P4 : PWR_OFF_PULSE : LOW
-        _io_expander_b->setDirection(4, true);
-        _io_expander_b->setPullMode(4, false);
-        _io_expander_b->setHighImpedance(4, false);
-        _io_expander_b->digitalWrite(4, false);
-        // P5 : nCHG_QC_EN : LOW -> quick charge
-        _io_expander_b->setDirection(5, true);
-        _io_expander_b->setPullMode(5, false);
-        _io_expander_b->setHighImpedance(5, false);
-        _io_expander_b->digitalWrite(5, false);
-        // P6 : CHG_STAT
-        _io_expander_b->setDirection(6, false);
-        _io_expander_b->setPullMode(6, true);
-        _io_expander_b->setHighImpedance(6, false);
-        // P7 : CHG_EN : LOW -> disable
-        _io_expander_b->setDirection(7, true);
-        _io_expander_b->setPullMode(7, false);
-        _io_expander_b->setHighImpedance(7, false);
-        _io_expander_b->digitalWrite(7, false);
-        delay(100);
-    }
+    auto& ioe = M5.getIOExpander(1);
+    ioe.digitalWrite(5, !enable);
 }
 
--- src/M5Tab5.h
+++ src/M5Tab5.h
@@ -4,8 +4,6 @@
  * SPDX-License-Identifier: MIT
  */
 #pragma once
-#include "utils/pi4ioe5v6408/pi4ioe5v6408.h"
-#include "utils/ina226/ina226.h"
 #include "utils/rx8130/rx8130.h"
 #include <M5GFX.h>
 #include <M5Unified.hpp>
@@ -20,11 +18,13 @@ public:
     void begin(bool clearAndDisableRTCInterrupt = true);
     void update();
 
-    INA226_Class INA226 = INA226_Class(0x41);
-    RX8130_Class RX8130;
+    // Deprecated: float getBatteryVoltage();
+    // Use: int32_t M5.Power.getBatteryLevel();
+    // Deprecated: float getBatteryCurrent();
+    // Use: int32_t M5.Power.getBatteryCurrent();
 
-    float getBatteryVoltage();
-    float getBatteryCurrent();
+    // === RX8130 ===
+    RX8130_Class RX8130;
 
     void setRtcTime(struct tm* time);
     void getRtcTime(struct tm* time);
@@ -36,30 +36,33 @@ public:
     void disableRtcIrq();
     uint8_t readRtcReg(uint8_t reg);
 
+    // === IO EXT A (0) ===
     void setRFPath(bool l_int_h_ext);
     void setSpeakerEnable(bool enable);
-    void setExt5VEnable(bool enable);
-    void setStamp5VEnable(bool enable);
+    // Deprecated: void setExt5VEnable(bool enable);
+    // Use: void M5.Power.setExtOutput(bool enable, m5::ext_port_mask_t::ext_PA);
+    void setStamp5VEnable(bool enable); // unused on non modified M5Tab5
     // resetLCD
     // resetTouch
     // resetCamera
     // headphoneDetect
 
+    // === IO EXT B (1) ===
     void setWLANPowerEnable(bool enable);
-    void setUSB5VEnable(bool enable);
-    void powerOff();
+    // unused
+    // unused
+    // Deprecated: void setUSB5VEnable(bool enable);
+    // Use: void M5.Power.setExtOutput(bool enable, m5::ext_port_mask_t::ext_USB);
+    // Deprecated: void powerOff();
+    // Use: void M5.Power.powerOff();
     void setQuickChargeEnable(bool enable);
-    bool getChargeState();
-    void setChargerEnable(bool enable);
+    // Deprecated: bool getChargeState();
+    // Use: Power_Class::is_charging_t M5.Power.isCharging();
+    // Deprecated: void setChargerEnable(bool enable);
+    // Use: void M5.Power.setBatteryCharge(bool enable);
 
 private:
-    PI4IOE5V6408_Class* _io_expander_a = nullptr;
-    PI4IOE5V6408_Class* _io_expander_b = nullptr;
-
     void io_expander_a_init();
-    void io_expander_b_init();
-
-    void ina226_init();
     void rx8130_init(bool clearAndDisableRTCInterrupt = true);
 };
 
